2bb856509e18534231642cb94c3ad699a5df2a64
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2021-09-23 19:37:29 -0700

    DOM Styling

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let header = document.querySelector("#page-header");
+    header.style.textAlign = "left"
+    header.style.backgroundColor = "black"
+let dogImages = document.querySelectorAll(".dog-image");
+    for (let i = 0; i < dogImages.length; i++){
+        dogImages[i].style.borderRadius = "50px"
+        dogImages[i].style.transform = "rotate(180deg)"
+    }
+let dogNames = document.querySelectorAll(".dog-name");
+    for (let i = 0; i < dogNames.length; i++){
+        dogNames[i].style.textAlign = "left"
+    }
+let footer = document.querySelector(".footer");
+    footer.style.color = "blue"
+    footer.style.borderStyle = "solid"
+    footer.style.backgroundColor = "black"
+let background = document.querySelector("body");
+    background.style.backgroundColor = "black";
+    background.style.color = "white"
 
-// Select some elements...
